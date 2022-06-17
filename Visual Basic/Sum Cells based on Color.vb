76a4e50f47d62c1c4521264c434c17e3bb005490
Nestor Padilla <40927322+pananess@users.noreply.github.com>
2022-06-17 09:11:44 -0500

    Visual Basic Excel tricks

---
+++ Visual Basic/Sum Cells based on Color.vb
@@ -0,0 +1,14 @@
+'Code created by Sumit Bansal from https://trumpexcel.com/
+'This VBA code created a function that can be used to sum cells based on color
+Function SumByColor(SumRange As Range, SumColor As Range)
+Dim SumColorValue As Integer
+Dim TotalSum As Long
+SumColorValue = SumColor.Interior.ColorIndex
+Set rCell = SumRange
+For Each rCell In SumRange
+If rCell.Interior.ColorIndex = SumColorValue Then
+TotalSum = TotalSum + rCell.Value
+End If
+Next rCell
+SumByColor = TotalSum
+End Function
